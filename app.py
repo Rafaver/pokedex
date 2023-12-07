eed955dc4632217b6eff1c61c888e809d9a1df0a
Rafaver <150279363+Rafaver@users.noreply.github.com>
2023-12-06 19:50:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
+
+
+def get_pokemon_data (pokemon):
+    url =f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    r =requests.get(url).json()
+    return r
 
 @app.route("/")
 def home():
@@ -9,6 +15,9 @@ def home():
 @app.route("/detalle")
 def detalle():
     return render_template('detalle.html')
+
+
+
 
 
 if __name__ == '__main__':
