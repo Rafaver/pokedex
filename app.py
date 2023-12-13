14788d2ed9bee88768feb46a62c90ed327413f7e
Rafaver <150279363+Rafaver@users.noreply.github.com>
2023-12-13 11:58:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped,mapped_column 
-
+from sqlalchemy.orm import Mapped, mapped_column 
 
 app = Flask(__name__)
 
@@ -11,13 +10,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
 db = SQLAlchemy(app)
 
 class Pokedex(db.Model):
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
-    name: Mapped[str] = mapped_column(db.String,nullable=False)
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
 
 with app.app_context():
     db.create_all()
-
 
 def get_pokemon_data(pokemon):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
@@ -28,6 +25,7 @@ def get_pokemon_data(pokemon):
         return image_url
     else:
         return None
+
 @app.route("/")
 def home():
     return render_template('pokemon.html')
@@ -36,31 +34,26 @@ def home():
 def search_pokemon(name):
     pokemon_data = get_pokemon_data(name.lower())
     if pokemon_data:
-        return jsonify({'image': pokemon_data})
+        return jsonify({'image': pokemon_data, 'name': name})
     else:
         return jsonify({'error': f'No se encontró ningún Pokémon con el nombre {name}'}), 404
 
-@app.route("/detalle")
-def detalle():
-    return render_template('detalle.html')
-
-
-
 @app.route("/insert")
 def insert():
-    new_pokemon = 'snorlax'
-    if new_pokemon:
-        obj = Pokedex(name=new_pokemon)
+    pokemon_name = request.args.get('name')
+    if pokemon_name:
+        obj = Pokedex(name=pokemon_name)
         db.session.add(obj)
         db.session.commit()
-    return 'pokemon agregado'
+        return 'Pokemon agregado'
+    else:
+        return 'No se proporcionó el nombre del Pokemon'
+
 
 @app.route("/select")
 def select():
-   lista_pokemon=Pokedex.query.all()
-   
+   lista_pokemon = Pokedex.query.all()
    return lista_pokemon
-
 
 if __name__ == '__main__':
     app.run(debug=True)
